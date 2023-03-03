baaa7929e16e38036e9e31753bbbd69e2bf847b1
botadsv1 <botadsv1@gmail.com>
2023-03-03 13:06:14 +0700

    up

--- config-server/config-server.js
+++ config-server/config-server.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 const server = "https://9980-192-166-244-67.ap.ngrok.io";
 // const server = "http://localhost";
-=======
-const server = "https://ddf6-185-225-234-31.ap.ngrok.io";
-// const server = "http://localhost";
->>>>>>> 9b1f72bbd8a9fa5a56655fffa74d5cea0806a01f
